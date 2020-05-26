7ba19771fa1700f5e6c372a94cf764a98bb5237e
filipestevao <filipe.estevao@gmail.com>
2020-05-26 11:33:41 -0300

    Added install.sh, icon and .desktop files

---
+++ install.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Copy files to the user's directory
+mkdir /home/$USER/.ElementaryGPUManager
+cp {*.py,intel-nvidia-icon.png} /home/$USER/.ElementaryGPUManager/
+cp elementary-gpu-manager.desktop /home/$USER/.local/share/applications/
+chmod +x /home/$USER/.ElementaryGPUManager/*.py
+chmod +x /home/$USER/.local/share/applications/elementary-gpu-manager.desktop
+
+# Install mesa-utils to use 'glxinfo' and 'glxgears'
+sudo apt install -y mesa-utils
+
+# Enable PPA
+sudo apt install -y software-properties-common
+
+# Install System76 PPA
+sudo apt-add-repository -ys ppa:system76-dev/stable
+
+# Update
+sudo apt update
+
+# Install System76 driver
+sudo apt install -y system76-driver-nvidia
+
